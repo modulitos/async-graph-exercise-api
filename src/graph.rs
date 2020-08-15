001b215b08d07b3d0b70997b2c318ebe4b70bc79
modulitos <modulitos@users.noreply.github.com>
2020-08-15 15:44:42 -0700

    formatting for ChildNode

--- src/graph.rs
+++ src/graph.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt::Debug;
 
 type NodeId = u32;
 
@@ -6,31 +7,44 @@ pub struct Graph {
     map: HashMap<NodeId, Node>,
 }
 
-// TODO: impl Debug for left and right to render custom "null" string
+#[derive(Hash)]
+pub struct ChildNode(Option<NodeId>);
+
 #[derive(Hash)]
 pub struct Node {
-    pub left: Option<NodeId>,
-    pub right: Option<NodeId>,
+    pub left: ChildNode,
+    pub right: ChildNode,
     pub score: u32,
+}
+
+impl Debug for ChildNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(n) = self.0 {
+            f.write_fmt(format_args!("{}", n))
+        } else {
+            f.write_str("null")
+        }
+    }
 }
 
 impl Graph {
     pub fn new() -> Self {
         let mut map = HashMap::new();
+        // TODO: read graph structure from a json file instead.
         map.insert(
             1,
             Node {
                 score: 2,
-                left: None,
-                right: Some(3),
+                left: ChildNode(None),
+                right: ChildNode(Some(3)),
             },
         );
         map.insert(
             3,
             Node {
                 score: 4,
-                left: None,
-                right: None,
+                left: ChildNode(None),
+                right: ChildNode(None),
             },
         );
         Self { map }
