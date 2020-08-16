a9f80a970abb500e00060561fbc8d93309c8eec7
modulitos <modulitos@users.noreply.github.com>
2020-08-16 00:21:44 -0700

    add graph tests

--- src/graph.rs
+++ src/graph.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::fmt::Debug;
 
 type NodeId = u32;
@@ -10,7 +10,6 @@ pub struct Graph {
 
 type Error = Box<dyn std::error::Error>;
 pub type Result<T, E = Error> = std::result::Result<T, E>;
-
 
 #[derive(Hash, Deserialize, Serialize)]
 pub struct ChildNode(Option<NodeId>);
@@ -43,7 +42,7 @@ impl Graph {
         let nodes: Vec<Node> = serde_json::from_str(&data)?;
 
         nodes.into_iter().for_each(|node| {
-           map.insert(node.id, node);
+            map.insert(node.id, node);
         });
 
         Ok(Self { map })
@@ -51,6 +50,31 @@ impl Graph {
 
     pub fn get(&self, id: NodeId) -> Option<&Node> {
         self.map.get(&id)
+    }
+
+    // this method helps with testing.
+    fn get_total(&self, id: NodeId) -> u32 {
+        let mut nodes_to_visit = VecDeque::new();
+        nodes_to_visit.push_back(id);
+        let mut total = 0;
+        loop {
+            if let Some(next_node_id) = nodes_to_visit.pop_front() {
+                let next_node = self
+                    .map
+                    .get(&next_node_id)
+                    .expect(&format!("non-existent node id: {}", next_node_id));
+                total += next_node.score;
+                if let Some(left_id) = next_node.left.0 {
+                    nodes_to_visit.push_back(left_id)
+                }
+                if let Some(right_id) = next_node.right.0 {
+                    nodes_to_visit.push_back(right_id)
+                }
+            } else {
+                // all nodes have been visited
+                return total;
+            }
+        }
     }
 }
 
@@ -74,5 +98,12 @@ fn graph_get() -> Result<()> {
 fn graph_get_non_existent() -> Result<()> {
     let graph = Graph::new()?;
     assert_eq!(graph.get(4).is_none(), true);
+    Ok(())
+}
+
+#[test]
+fn graph_sum() -> Result<()> {
+    let graph = Graph::new()?;
+    assert_eq!(graph.get_total(1), 1650);
     Ok(())
 }
